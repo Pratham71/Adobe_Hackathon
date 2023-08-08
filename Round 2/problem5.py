f949e5bcea77e4f9d46d21e36c2e5691b4478427
neev0511 <allofbakwas@gmail.com>
2023-08-08 16:39:23 +0530

    Problem 5

--- Round 2/problem5.py
+++ Round 2/problem5.py
@@ -1,25 +1,9 @@
-# STRUCTURE
-# 1) Loop through each image get list of entity and coordinte location
-# 2) For{1} each entity in that image loop{2} through all the other images
-#    with entity that is the same and and obtain similarity rating
-# 3) Compare similarity as top 3 and make subfolder and paste cropped images in it
-
-# TODO
-# ----------DONE get_list_of_entity_with_coords() ==> get list of entities with their
-#                respective coordinates
-# ----------DONE make_folder() ==> make a folder for respective enitity
-# ----------DONE return_folder_and_image_name() ==> returns name of folder
-#                & name of image to save the picture in
-# ----------DONE crop_entity() ==> crop the images from respective coordinates
-# analyze_similarity() ==> get similarity between original entity and other images
-#                              and get the top 3 with highest similarity
-# Esstimate Time To Complete ==> 2 hours
-
 # METHOD USED for problem 5
-# 1) Crop all similar entities --> DONE
-# 2) Analyze TOP 3 similar entities --> DONE
-# 3) Rename TOP 3 similar entities
-# 4) Delete not similar entities
+# 1) Making folders for each image and in that sub folders for its entities
+# 2) Crop all similar entities and save them in respective folder
+# 3) Analyze TOP 3 similar entities
+# 4) Rename TOP 3 similar entities
+# 5) Delete not similar entities
 
 from ultralytics import YOLO
 import os
@@ -95,21 +79,6 @@ def get_list_of_entity_with_coords(images_file):
     return ideal_images_dir
 
 
-def return_folder_and_image_name(string, count=0):
-    # returns name of folder
-    # and name of image to save the picture in
-
-    folder_name = ((string.split("-")[-1]).split(".")
-                   [0]+"/").replace("All_Images", "problem5_output")
-    entity = string.split("-")[0].lower()+string.split("-")[1]
-    folder_name += entity
-    if count == 0:
-        img_name = folder_name+"/"+entity
-    else:
-        img_name = folder_name+"/"+"top"+str(count)
-    return folder_name, img_name
-
-
 def clean_coords(coordinates):
     old_coords = coordinates[1:-1]
     old_coords = old_coords.split(".")
@@ -154,12 +123,17 @@ def crop_lists(original, lists_of_images, path):
 
 
 def rename(top1_name, top2_name, top3_name, path):
+    path = os.getcwd().replace("\\", "/")+"/"+path
     os.rename(path+top1_name, path+"top1-crop.jpg")
     os.rename(path+top2_name, path+"top2-crop.jpg")
-    os.rename(path+top3_name, path+"top3-crop.jpg")
+    if not top3_name:
+        pass
+    else:
+        os.rename(path+top3_name, path+"top3-crop.jpg")
 
 
 def delete(path, files):
+    path = os.getcwd().replace("\\", "/")+"/"+path
     for file in files:
         if "-crop" not in file:
             os.remove(path+file)
@@ -176,22 +150,34 @@ def similarity_bwt_two_pictures(image1, image2):
 def get_top_three_indexes(listing):
     top1 = max(listing)
     top2 = -1
-    top3 = -1
     index1 = 0
     index2 = 0
-    index3 = 0
-    count = 0
-    for num in listing:
-        if num == top1:
-            index1 = count
-        elif num > top2:
-            top2 = num
-            index2 = count
-        elif num > top3:
-            top3 = count
-            index3 = count
-        count += 1
-    return index1, index2, index3
+    if len(listing) == 2:
+        index3 = -100
+        count = 0
+        for num in listing:
+            if num == top1:
+                index1 = count
+            elif num > top2:
+                top2 = num
+                index2 = count
+            count += 1
+        return index1, index2, index3
+    else:
+        top3 = -1
+        index3 = 0
+        count = 0
+        for num in listing:
+            if num == top1:
+                index1 = count
+            elif num > top2:
+                top2 = num
+                index2 = count
+            elif num > top3:
+                top3 = count
+                index3 = count
+            count += 1
+        return index1, index2, index3
 
 
 def analyze(path, files):
@@ -209,7 +195,10 @@ def analyze(path, files):
     for image in similar:
         similarity.append(similarity_bwt_two_pictures(original, image))
     i1, i2, i3 = get_top_three_indexes(similarity)
-    return path+files[i1], path+files[i2], path+files[i3]
+    if i3 == -100:
+        return files[i1], files[i2], False
+    else:
+        return files[i1], files[i2], files[i3]
 
 
 def analyze_similarity(path):
@@ -225,8 +214,7 @@ def analyze_similarity(path):
     else:
         top1_name, top2_name, top3_name = analyze(path, files)
         rename(top1_name, top2_name, top3_name, path)
-        delete(list_files(path, files))
-        pass
+        delete(path, list_files(path))
 
 
 # Initialise Problem & Output Folder
@@ -246,26 +234,34 @@ create_directory(output_path)
 list_with_file_entity_coords = get_list_of_entity_with_coords(files)
 values = list(list_with_file_entity_coords.values())
 
-for img_list in values:
-    for string in img_list:
-        # Make a folder for respective image
-        img_name = ((string.split("-")[-1]).split(".")
-                    [0]+"/").replace("All_Images", "problem5_output")
-        create_directory(img_name)
-        entity = string.split("-")[0]
-        entity_for_folder = string.split("-")[0].lower()+string.split("-")[1]
 
-        # Make a folder for respective entity
-        create_directory(img_name+entity_for_folder)
+def main():
+    for img_list in values:
+        for string in img_list:
+            # Make a folder for respective image
+            img_name = ((string.split("-")[-1]).split(".")
+                        [0]+"/").replace("All_Images", "problem5_output")
+            create_directory(img_name)
+            entity = string.split("-")[0]
+            entity_for_folder = string.split(
+                "-")[0].lower()+string.split("-")[1]
 
-        # Initialise List
-        similar_entities_list = []
-        for similar_list in values:
-            if img_list != similar_list:
-                for similar_string in similar_list:
-                    similar_entity = similar_string.split("-")[0]
-                    if similar_entity == entity:
-                        similar_entities_list.append(similar_string)
-        crop_lists(string, similar_entities_list,
-                   img_name+entity_for_folder+"/")
-        analyze_similarity(img_name+entity_for_folder+"/")
+            # Make a folder for respective entity
+            create_directory(img_name+entity_for_folder)
+
+            # Initialise List
+            similar_entities_list = []
+            for similar_list in values:
+                if img_list != similar_list:
+                    for similar_string in similar_list:
+                        similar_entity = similar_string.split("-")[0]
+                        if similar_entity == entity:
+                            similar_entities_list.append(similar_string)
+            crop_lists(string, similar_entities_list,
+                       img_name+entity_for_folder+"/")
+            analyze_similarity(img_name+entity_for_folder+"/")
+
+
+if __name__ == "__main__":
+    main()
+    # Baseline 23 mins 23.269339 seconds
